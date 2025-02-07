cb8cdfbde6893f114dc2ecbc35581ee7b8567e7e
Siva Regunathan <siva.regunathan@getmojo.ai>
2025-02-07 23:09:39 +0530

    Added Resume Download button

--- js/script.js
+++ js/script.js
@@ -7,25 +7,25 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 
-  // Adjust threshold based on screen width
-  const thresholdValue = window.innerWidth < 768 ? 0.1 : 0.2;
-
-  const observerOptions = {
-    threshold: thresholdValue,
-    rootMargin: "0px 0px -50px 0px" // This can help trigger earlier if needed
-  };
-
-  const sections = document.querySelectorAll('.section');
-  const sectionObserver = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('visible');
-        observer.unobserve(entry.target);
-      }
-    });
-  }, observerOptions);
-
-  sections.forEach(section => {
-    sectionObserver.observe(section);
-  });
+//  // Adjust threshold based on screen width
+//  const thresholdValue = window.innerWidth < 768 ? 0.1 : 0.2;
+//
+//  const observerOptions = {
+//    threshold: thresholdValue,
+//    rootMargin: "0px 0px -50px 0px" // This can help trigger earlier if needed
+//  };
+//
+//  const sections = document.querySelectorAll('.section');
+//  const sectionObserver = new IntersectionObserver((entries, observer) => {
+//    entries.forEach(entry => {
+//      if (entry.isIntersecting) {
+//        entry.target.classList.add('visible');
+//        observer.unobserve(entry.target);
+//      }
+//    });
+//  }, observerOptions);
+//
+//  sections.forEach(section => {
+//    sectionObserver.observe(section);
+//  });
 });
